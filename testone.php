0dfbd99af58a1b258241fed81626246751230dac
devanshmishra <devanshmishra872@gmail.com>
2024-05-04 22:25:29 +0530

    update my code with some changes

--- testone.php
+++ testone.php
@@ -1,17 +1,39 @@
 <?php 
 
-// first commit 
-#for the testing purpose 
+// used to hold variable to print the value .
+$var_test="hello this is the testing variable value";
+echo $var_test; 
 
-echo "<h1>hello, world!</h1>";
+// this is used to concatenate the two string
 
-function greet($data,$value){
+$var_test_two="this is the second variable value for testing the concatenation";
+echo "<br/>";
+echo "<br/>";
+echo $var_test ."-" . $var_test_two;
+echo "<br/>"; 
 
-	echo "hello"." ! ".$data." you are the best ".$value;
+
+$age=23;
+if ($age<18) {
+	
+   echo "sorry! you can not give the vote in upcoming election";
+}else{
+   
+   echo "yes! you can give the vote in upcoming election";
+
 }
+// shorthand for the if else statment
+$age = 25;  
 
-greet("devansh","person");
-// first commit 
+echo ($age > 20) ? "Yes, you are awesome" : "You are not awesome";
+
+
+$a=3;
+$b=1;
+$b+=$a;
+
+echo $b;
+
 
 
 ?>
